96be96aa994ae65123ddb41b7a74ba6179d0c8fa
jd-hilton <j+work@hiltons.org.uk>
2020-09-25 11:19:22 +0100

    Rook can move up and down the board but not past other pieces

--- src/main/java/training/chessington/model/pieces/Rook.java
+++ src/main/java/training/chessington/model/pieces/Rook.java
@@ -15,6 +15,45 @@ public class Rook extends AbstractPiece {
 
     @Override
     public List<Move> getAllowedMoves(Coordinates from, Board board) {
-        return new ArrayList<>();
+        List<Move> allowedMoves = new ArrayList<>();
+        Coordinates to = null;
+        moveUpTheBoard(board, from, allowedMoves);
+        moveDownTheBoard(board, from, allowedMoves);
+
+        return allowedMoves;
+    }
+
+    public void moveUpTheBoard (Board board, Coordinates from, List<Move> allowedMoves) {
+        boolean pieceInWay = false;
+        for (int i = from.getRow() - 1; i >= 0; i--) {
+            Coordinates to = new Coordinates(i, from.getCol());
+            if (!pieceInWay) {
+                if (board.get(to) == null) {
+                    allowedMoves.add(new Move(from, to));
+                } else if (board.get(to) != null) {
+                    if (board.get(to).getColour() != this.getColour()) {
+                        allowedMoves.add(new Move(from, to));
+                    }
+                    pieceInWay = true;
+                }
+            }
+        }
+    }
+
+    public void moveDownTheBoard (Board board, Coordinates from, List<Move> allowedMoves) {
+        boolean pieceInWay = false;
+        for (int i = from.getRow() + 1; i <= 7; i++) {
+            Coordinates to = new Coordinates(i, from.getCol());
+            if (!pieceInWay) {
+                if (board.get(to) == null) {
+                    allowedMoves.add(new Move(from, to));
+                } else if (board.get(to) != null) {
+                    if (board.get(to).getColour() != this.getColour()) {
+                        allowedMoves.add(new Move(from, to));
+                    }
+                    pieceInWay = true;
+                }
+            }
+        }
     }
 }
--- src/test/java/training/chessington/model/pieces/RookTest.java
+++ src/test/java/training/chessington/model/pieces/RookTest.java
@@ -1,7 +1,146 @@
 package training.chessington.model.pieces;
 
+import org.junit.Test;
+import training.chessington.model.Board;
+import training.chessington.model.Coordinates;
+import training.chessington.model.Move;
+import training.chessington.model.PlayerColour;
+
+import java.util.List;
+
 import static training.chessington.model.pieces.PieceAssert.*;
 import static org.assertj.core.api.Assertions.*;
 
 public class RookTest {
+    @Test
+    public void rookNotAbleToMovePastAllyPiece() {
+        // Arrange
+        Board board = Board.empty();
+        Piece rook = new Rook(PlayerColour.WHITE);
+        Coordinates coords = new Coordinates(7,7);
+        board.placePiece(coords, rook);
+        Piece pawn = new Pawn(PlayerColour.WHITE);
+        Coordinates pawnCoords = new Coordinates(4,7);
+        board.placePiece(pawnCoords, pawn);
+
+        // Act
+        List<Move> moves = rook.getAllowedMoves(coords, board);
+
+        // Assert
+        assertThat(moves).contains(new Move(coords, coords.plus(-1,0)));
+        assertThat(moves).contains(new Move(coords, coords.plus(-2,0)));
+        assertThat(moves).doesNotContain(new Move(coords, coords.plus(-3,0)));
+        assertThat(moves).doesNotContain(new Move(coords, coords.plus(-4,0)));
+        assertThat(moves).doesNotContain(new Move(coords, coords.plus(-5,0)));
+        assertThat(moves).doesNotContain(new Move(coords, coords.plus(-6,0)));
+        assertThat(moves).doesNotContain(new Move(coords, coords.plus(-7,0)));
+    }
+
+    @Test
+    public void rookNotAbleToMovePastEnemyPiece() {
+        // Arrange
+        Board board = Board.empty();
+        Piece rook = new Rook(PlayerColour.WHITE);
+        Coordinates coords = new Coordinates(7,7);
+        board.placePiece(coords, rook);
+        Piece pawn = new Pawn(PlayerColour.BLACK);
+        Coordinates pawnCoords = new Coordinates(4,7);
+        board.placePiece(pawnCoords, pawn);
+
+        // Act
+        List<Move> moves = rook.getAllowedMoves(coords, board);
+
+        // Assert
+        assertThat(moves).contains(new Move(coords, coords.plus(-1,0)));
+        assertThat(moves).contains(new Move(coords, coords.plus(-2,0)));
+        assertThat(moves).contains(new Move(coords, coords.plus(-3,0)));
+        assertThat(moves).doesNotContain(new Move(coords, coords.plus(-4,0)));
+        assertThat(moves).doesNotContain(new Move(coords, coords.plus(-5,0)));
+        assertThat(moves).doesNotContain(new Move(coords, coords.plus(-6,0)));
+        assertThat(moves).doesNotContain(new Move(coords, coords.plus(-7,0)));
+    }
+
+    @Test
+    public void rookCanMoveUpTheBoardWhite() {
+        // Arrange
+        Board board = Board.empty();
+        Piece rook = new Rook(PlayerColour.WHITE);
+        Coordinates coords = new Coordinates(7,7);
+        board.placePiece(coords, rook);
+
+        // Act
+        List<Move> moves = rook.getAllowedMoves(coords, board);
+
+        // Assert
+        assertThat(moves).contains(new Move(coords, coords.plus(-1,0)));
+        assertThat(moves).contains(new Move(coords, coords.plus(-2,0)));
+        assertThat(moves).contains(new Move(coords, coords.plus(-3,0)));
+        assertThat(moves).contains(new Move(coords, coords.plus(-4,0)));
+        assertThat(moves).contains(new Move(coords, coords.plus(-5,0)));
+        assertThat(moves).contains(new Move(coords, coords.plus(-6,0)));
+        assertThat(moves).contains(new Move(coords, coords.plus(-7,0)));
+    }
+
+    @Test
+    public void rookCanMoveUpTheBoardBlack() {
+        // Arrange
+        Board board = Board.empty();
+        Piece rook = new Rook(PlayerColour.BLACK);
+        Coordinates coords = new Coordinates(7,7);
+        board.placePiece(coords, rook);
+
+        // Act
+        List<Move> moves = rook.getAllowedMoves(coords, board);
+
+        // Assert
+        assertThat(moves).contains(new Move(coords, coords.plus(-1,0)));
+        assertThat(moves).contains(new Move(coords, coords.plus(-2,0)));
+        assertThat(moves).contains(new Move(coords, coords.plus(-3,0)));
+        assertThat(moves).contains(new Move(coords, coords.plus(-4,0)));
+        assertThat(moves).contains(new Move(coords, coords.plus(-5,0)));
+        assertThat(moves).contains(new Move(coords, coords.plus(-6,0)));
+        assertThat(moves).contains(new Move(coords, coords.plus(-7,0)));
+    }
+
+    @Test
+    public void rookCanMoveDownTheBoardWhite() {
+        // Arrange
+        Board board = Board.empty();
+        Piece rook = new Rook(PlayerColour.WHITE);
+        Coordinates coords = new Coordinates(0,7);
+        board.placePiece(coords, rook);
+
+        // Act
+        List<Move> moves = rook.getAllowedMoves(coords, board);
+
+        // Assert
+        assertThat(moves).contains(new Move(coords, coords.plus(+1,0)));
+        assertThat(moves).contains(new Move(coords, coords.plus(+2,0)));
+        assertThat(moves).contains(new Move(coords, coords.plus(+3,0)));
+        assertThat(moves).contains(new Move(coords, coords.plus(+4,0)));
+        assertThat(moves).contains(new Move(coords, coords.plus(+5,0)));
+        assertThat(moves).contains(new Move(coords, coords.plus(+6,0)));
+        assertThat(moves).contains(new Move(coords, coords.plus(+7,0)));
+    }
+
+    @Test
+    public void rookCanMoveDownTheBoardBlack() {
+        // Arrange
+        Board board = Board.empty();
+        Piece rook = new Rook(PlayerColour.BLACK);
+        Coordinates coords = new Coordinates(0,7);
+        board.placePiece(coords, rook);
+
+        // Act
+        List<Move> moves = rook.getAllowedMoves(coords, board);
+
+        // Assert
+        assertThat(moves).contains(new Move(coords, coords.plus(+1,0)));
+        assertThat(moves).contains(new Move(coords, coords.plus(+2,0)));
+        assertThat(moves).contains(new Move(coords, coords.plus(+3,0)));
+        assertThat(moves).contains(new Move(coords, coords.plus(+4,0)));
+        assertThat(moves).contains(new Move(coords, coords.plus(+5,0)));
+        assertThat(moves).contains(new Move(coords, coords.plus(+6,0)));
+        assertThat(moves).contains(new Move(coords, coords.plus(+7,0)));
+    }
 }
